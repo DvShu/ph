f1639501d15b1dcd762e0e7ff8bd2b6c1b94db2b
haoran.shu <haoran.shu@laifusi.com>
2021-05-21 14:25:24 +0800

    feat: 新增 terminal-icons
    Signed-off-by: haoran.shu <haoran.shu@laifusi.com>

---
+++ packages/terminal-icons/index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * 所有的图标集
+ */
+declare const icons: {
+    /** 图标：i，颜色为蓝色 */
+    info: string;
+    /** 图标：√，颜色为绿色 */
+    success: string;
+    /** 图标：‼，颜色为黄色 */
+    warning: string;
+    /** 图标：×，颜色为红色 */
+    error: string;
+};
+export = icons;
---
+++ packages/terminal-icons/test.js
@@ -0,0 +1,6 @@
+const icons = require('./index')
+
+console.log(icons.error, 'error')
+console.log(icons.info, 'info')
+console.log(icons.success, 'success')
+console.log(icons.warning, 'warning')
